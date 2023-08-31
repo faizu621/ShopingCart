38f362641ed613b9e17d88ea0c51ce9142921015
faizu621 <faizu78601@gmail.com>
2023-08-31 17:34:16 +0530

    image path changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import {FaShoppingCart} from "react-icons/fa"
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import Image from "../images.png"
 
 const Navbar = () => {
   const {cart} =useSelector((state)=>state);
@@ -9,7 +10,7 @@ const Navbar = () => {
     <div className=" bg-slate-800 h-20 flex items-center " >
       <div className="w-11/12 max-w-[1100px] mx-auto flex justify-between  ">
          <NavLink to="/">
-          <img src="../images.png" className="h-[56px] ml-4 " />
+          <img src={Image} className="h-[56px] ml-4 " />
          </NavLink>
         <div className=" text-white flex gap-x-2 items-center ">
           <NavLink to="/">
